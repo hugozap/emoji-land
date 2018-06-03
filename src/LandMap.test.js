d4048277494b5252807414a586574c3758b7b821
Hugo Zapata <hugozap@gmail.com>
2018-06-02 23:48:18 -0500

    LandMap lat,lon recalculation tests pass

--- src/LandMap.test.js
+++ src/LandMap.test.js
@@ -24,9 +24,27 @@ describe('LandMap tests', ()=>{
 		})
 	})
 
-	it('Move n pixels to the right advances to the next parcel', ()=> {
+	it('Move -199 pixels goes to the next right parcel, when parcelLength is 200', ()=> {
 		var lm = new LandMap({parcelLength:200})
-		lm.move({x:200, y:0})
+		lm.move({x:-199, y:0})
+		const active = lm.getActiveParcels()
+		//latitude increases by 1
+		expect(active).toEqual({
+			A:{lat:-1-1, lon:-1},
+			B:{lat: 0-1, lon:-1},
+			C:{lat: 1-1, lon:-1},
+			D:{lat:-1-1, lon: 0},
+			E:{lat: 0-1 ,lon: 0},
+			F:{lat: 1-1, lon: 0},
+			G:{lat:-1-1, lon: 1},
+			H:{lat: 0-1, lon: 1},
+			I:{lat: 1-1, lon: 1}
+		})
+	})
+
+	it('Move n pixels to the right goes to the prev parcel', ()=> {
+		var lm = new LandMap({parcelLength:200})
+		lm.move({x:201, y:0})
 		const active = lm.getActiveParcels()
 		//latitude increases by 1
 		expect(active).toEqual({
@@ -41,4 +59,42 @@ describe('LandMap tests', ()=>{
 			I:{lat: 1+1, lon: 1}
 		})
 	})
+
+	it('Move n pixels up goes to the prev parcel', ()=> {
+		var lm = new LandMap({parcelLength:200})
+		lm.move({x:0, y:-199})
+		const active = lm.getActiveParcels()
+		//latitude increases by 1
+		expect(active).toEqual({
+			A:{lat:-1, lon:-1-1},
+			B:{lat: 0, lon:-1-1},
+			C:{lat: 1, lon:-1-1},
+			D:{lat:-1, lon: 0-1},
+			E:{lat: 0 ,lon: 0-1},
+			F:{lat: 1, lon: 0-1},
+			G:{lat:-1, lon: 1-1},
+			H:{lat: 0, lon: 1-1},
+			I:{lat: 1, lon: 1-1}
+		})
+	})
+
+	it('Move n pixels down goes to the next parcel', ()=> {
+		var lm = new LandMap({parcelLength:200})
+		lm.move({x:0, y:201})
+		const active = lm.getActiveParcels()
+		//latitude increases by 1
+		expect(active).toEqual({
+			A:{lat:-1, lon:-1+1},
+			B:{lat: 0, lon:-1+1},
+			C:{lat: 1, lon:-1+1},
+			D:{lat:-1, lon: 0+1},
+			E:{lat: 0 ,lon: 0+1},
+			F:{lat: 1, lon: 0+1},
+			G:{lat:-1, lon: 1+1},
+			H:{lat: 0, lon: 1+1},
+			I:{lat: 1, lon: 1+1}
+		})
+	})
+
+
 })
